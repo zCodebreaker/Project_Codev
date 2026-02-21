689d8e9c764cf75cd934307fdaa862db14d075ab
Adrya <Adrya@ADRIAN>
2026-02-21 18:21:42 -0300

    Adicionar arquivos de projeto.

---
+++ Codev/LoginWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Interop;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Codev
+{
+    /// <summary>
+    /// Lógica interna para LoginWindow.xaml
+    /// </summary>
+    public partial class LoginWindow
+    {
+        public LoginWindow()
+        {
+            InitializeComponent();
+        }
+        private void Fechar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
